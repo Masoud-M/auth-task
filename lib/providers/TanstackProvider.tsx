98cad5b3f8cb3e0e2bf8fb5a1b187ac4b77fddd5
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 12:01:35 +0330

    create: Tanstack react query provider

---
+++ lib/providers/TanstackProvider.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function TanstackProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 1000,
+        refetchInterval: 30 * 1000,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        {children}
+      </HydrationBoundary>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
