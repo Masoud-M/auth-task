bf41c0de7c6627eecd4800ff6d8941ce4ee24de4
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 13:32:49 +0330

    update: useSignup hook
    using the server action wrapped inside a function to be used on the client side

--- lib/hooks/useSignup.ts
+++ lib/hooks/useSignup.ts
@@ -1,11 +1,12 @@
-import { signupAction } from "../server/users";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { signupRequest } from "../utils/api";
 
 export function useSignup() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: signupAction,
+    mutationFn: (values: { email: string; password: string }) =>
+      signupRequest(values),
     onSuccess: async () => {
       await queryClient.invalidateQueries();
     },
