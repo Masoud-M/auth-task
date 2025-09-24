b3ca0f95e2921b283f0c2357b387d88232524fe1
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 11:23:27 +0330

    update: signup page styling

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function SignupPage() {
   return (
-    <section className="flex justify-center items-center w-full min-h-svh">
+    <section className="flex justify-center items-center p-4 w-full min-h-svh">
       <div className="bg-neutral-100 p-6 md:p-10 rounded-md w-full max-w-sm">
         <h1 className="place-self-start mb-8 font-semibold text-2xl">Signup</h1>
         <form>
