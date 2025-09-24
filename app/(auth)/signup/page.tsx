c68407a04a2aa6702e8367f789a0f5843979e2e9
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 13:14:56 +0330

    update: signup page
    refactored the code to use react hook form and zod

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,38 +1,108 @@
+"use client";
+
 import Button from "@/components/auth/Button";
 import Input from "@/components/auth/Input";
 import Label from "@/components/auth/Label";
 import Wrapper from "@/components/auth/Wrapper";
 import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useSignup } from "@/lib/hooks/useSignup";
+
+const formSchema = z
+  .object({
+    email: z.string().email({ message: "Please enter a valid email address." }),
+    password: z.string().min(8, {
+      message: "Password must be at least 8 characters.",
+    }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters." }),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      });
+    }
+  });
 
 export default function SignupPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const { mutate: signup, status } = useSignup();
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    signup(values);
+  }
+
   return (
     <Wrapper title="Signup">
-      <form>
-        <div className="flex flex-col gap-6">
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              placeholder="email@example.com"
-              id="email"
-              required
-            />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input type="password" id="password" required />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Label htmlFor="password-confirm">Confirm your password</Label>
-            <Input type="password" id="password-confirm" required />
-          </div>
-          <div className="flex flex-col gap-2">
-            <Button type="submit" className="w-full">
-              Signup
-            </Button>
-          </div>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+        {/* Email */}
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            placeholder="email@example.com"
+            {...register("email")}
+          />
+          {errors.email && (
+            <span className="text-red-500 text-sm">{errors.email.message}</span>
+          )}
         </div>
+
+        {/* Password */}
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" type="password" {...register("password")} />
+          {errors.password && (
+            <span className="text-red-500 text-sm">
+              {errors.password.message}
+            </span>
+          )}
+        </div>
+
+        {/* Confirm Password */}
+        <div className="flex flex-col gap-2">
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            id="confirmPassword"
+            type="password"
+            {...register("confirmPassword")}
+          />
+          {errors.confirmPassword && (
+            <span className="text-red-500 text-sm">
+              {errors.confirmPassword.message}
+            </span>
+          )}
+        </div>
+
+        {/* Submit */}
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={status === "pending"}
+        >
+          {status === "pending" ? "Loading..." : "Signup"}
+        </Button>
       </form>
+
       <p className="mt-6 text-neutral-500 text-sm">
         Already have an account?
         <Link href="/login" className="ml-1 underline underline-offset-4">
