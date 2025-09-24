32ac6b8d252a51a76492e1177958028a51d32b77
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 14:31:21 +0330

    update: Wrapper component styling

--- components/auth/Wrapper.tsx
+++ components/auth/Wrapper.tsx
@@ -6,7 +6,7 @@ export default function Wrapper({
   children: React.ReactNode;
 }>) {
   return (
-    <section className="flex justify-center items-center p-4 w-full min-h-svh">
+    <section className="flex justify-center items-center p-4 w-full min-h-[80dvh]">
       <div className="bg-neutral-100 p-6 md:p-10 rounded-md w-full max-w-sm">
         <h1 className="place-self-start mb-8 font-semibold text-2xl">
           {title}
