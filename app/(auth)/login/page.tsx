5081897b307ecdd6c78051c68d0765b6b2cdbbef
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 12:41:38 +0330

    update: login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,41 @@
+import Button from "@/components/auth/Button";
+import Input from "@/components/auth/Input";
+import Label from "@/components/auth/Label";
+import Wrapper from "@/components/auth/Wrapper";
 import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <section>
-      <div className="flex flex-col justify-center items-center mx-auto px-4 py-24 max-w-[380px] min-h-[90dvh]">
-        <h1 className="place-self-start mb-8 font-semibold text-3xl">Login</h1>
-        INPUTS
-        <p className="mt-6 text-neutral-500 text-sm">
-          Don't have an account?
-          <Link href="/signup" className="ml-1 underline underline-offset-4">
-            Signup
-          </Link>
-        </p>
-      </div>
-    </section>
+    <Wrapper title="Login">
+      <form>
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              placeholder="email@example.com"
+              id="email"
+              required
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input type="password" id="password" required />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Button variant="outline" type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+        </div>
+      </form>
+      <p className="mt-6 text-neutral-500 text-sm">
+        Don't have an account?
+        <Link href="/signup" className="ml-1 underline underline-offset-4">
+          Signup
+        </Link>
+      </p>
+    </Wrapper>
   );
 }
