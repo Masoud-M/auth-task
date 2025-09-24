f2219e15fb51978dda29f0690833925d25a5a829
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 13:12:35 +0330

    update: Input component
    forwarding ref so that react hook form can use them for error handling

--- components/auth/Input.tsx
+++ components/auth/Input.tsx
@@ -1,18 +1,21 @@
 import { twMerge } from "tailwind-merge";
+import React from "react";
 
-export default function Input({
-  className,
-  type,
-  ...props
-}: React.ComponentProps<"input">) {
-  return (
-    <input
-      type={type}
-      {...props}
-      className={twMerge(
-        "bg-white disabled:opacity-50 shadow-xs px-3 py-1 border rounded-md w-full min-w-0 h-9 md:text-sm text-base transition-[color,box-shadow] disabled:cursor-not-allowed disabled:pointer-events-none",
-        className
-      )}
-    />
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type={type}
+        {...props}
+        className={twMerge(
+          "bg-white disabled:opacity-50 shadow-xs px-3 py-1 border rounded-md w-full min-w-0 h-9 md:text-sm text-base transition-[color,box-shadow] disabled:cursor-not-allowed disabled:pointer-events-none",
+          className
+        )}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+export default Input;
