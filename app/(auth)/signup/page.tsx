85cb304b78c9e4b551751b979a982ed2d1f35c5c
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 10:47:47 +0330

    update: signup page
    add inputs for email and password

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -8,14 +8,24 @@ export default function SignupPage() {
       <div className="bg-neutral-100 p-6 md:p-10 rounded-md w-full max-w-sm">
         <h1 className="place-self-start mb-8 font-semibold text-2xl">Signup</h1>
         <form>
-          <div className="flex flex-col gap-4">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              placeholder="email@example.com"
-              id="email"
-              required
-            />
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                type="email"
+                placeholder="email@example.com"
+                id="email"
+                required
+              />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input type="password" id="password" required />
+            </div>
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="password-confirm">Confirm your password</Label>
+              <Input type="password" id="password-confirm" required />
+            </div>
           </div>
         </form>
         <p className="mt-6 text-neutral-500 text-sm">
