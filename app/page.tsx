983b5d60540614dc1428bd53ce11f6f4a43776d1
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-24 14:17:36 +0330

    update: home page styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Container from "@/components/Container";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Homepage. Accessible by all users.</h1>;
+  return (
+    <Container>
+      <h1 className="font-semibold text-4xl">
+        Homepage. Accessible by all users.
+      </h1>
+    </Container>
+  );
 }
