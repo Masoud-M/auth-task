12c4f12e65807a00e92c68f2080119ae488f3631
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-25 12:13:15 +0330

    refactor: deleted tanstak react query and all the related hooks and functions

--- lib/hooks/useGetUser.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getUser } from "../server/users";
-
-export function useGetUser() {
-  return useQuery({
-    queryFn: async () => await getUser(),
-    queryKey: ["user"],
-  });
-}
