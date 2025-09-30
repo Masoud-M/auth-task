1c2d2463b95fcf02178b55f245ac3f25bbab0121
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2025-09-30 22:26:43 +0330

    update: Heading component styling
    added padding

--- components/ui/Heading.tsx
+++ components/ui/Heading.tsx
@@ -1,3 +1,3 @@
 export default function Heading({ text }: { text: string }) {
-  return <h1 className="font-semibold text-4xl">{text}</h1>;
+  return <h1 className="p-4 font-semibold text-4xl">{text}</h1>;
 }
